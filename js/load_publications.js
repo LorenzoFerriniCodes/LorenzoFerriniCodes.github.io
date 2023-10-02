b04cd14cefd46b22ea7c6d4cd793f08cfdf94f74
lorenzoferrini <lorenzo.ferrini@pal-robotics.com>
2023-10-03 01:53:03 +0200

    added publications

---
+++ js/load_publications.js
@@ -0,0 +1,32 @@
+function loadPublications(year) {
+    fetch(`pub/${year}.json`) // Adjust the path to your JSON files
+        .then(response => response.json())
+        .then(data => {
+            const publicationsList = document.getElementById('publications');
+            const publicationsYear = document.createElement('div');
+            publicationsYear.className = 'publication-year';
+            publicationsYear.innerHTML = `<h3>${year}</h3>`;
+            publicationsList.appendChild(publicationsYear);
+
+            data.publications.forEach(publication => {
+                const publicationHtml = document.createElement('div');
+                publicationHtml.className = 'publication';
+                publicationHtml.innerHTML = `<em>${publication.title}</em>, ${publication.author}, ${publication.year}`;
+                if (publication.hasOwnProperty("pub_link")){
+                    publicationHtml.innerHTML += `, [<a href=${publication.pub_link}>pdf</a>]`
+                }
+                if (publication.hasOwnProperty("code_repo")){
+                    publicationHtml.innerHTML += `, [<a href=${publication.code_repo}>code</a>]`
+                }
+                publicationsList.appendChild(publicationHtml);
+            });
+        })
+        .catch(error => {
+            console.error(`Error loading publications: ${error}`);
+        });
+}
+
+for (let year = new Date().getFullYear(); year > 2021; year--){
+    loadPublications(year);
+}
+
